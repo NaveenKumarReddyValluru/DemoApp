b48c122f4d69dc66bc0ac327c7f0e033d73b0939
Naveen <naveenreddy1107@gmail.com>
2023-10-17 14:54:22 +0530

    First Commit

---
+++ DemoApp/src/in/ineuron/main/DemoApp.java
@@ -0,0 +1,18 @@
+package in.ineuron.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+	
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+		System.out.println("naveenReddy");
+	}
+
+}
